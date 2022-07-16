e76f4a80d6431568f57ed6d23fde0d30993d9be1
Laciann <abhishek.acharya@apexcollege.edu.np>
2022-07-16 20:13:20 +0545

    Updates

---
+++ precache-manifest.3b6ac99d8abb9ea99b46ba17a958303c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "641ae1d71b5f906e20e4",
+    "url": "/react-day-planner/static/css/main.90d008ef.chunk.css"
+  },
+  {
+    "revision": "641ae1d71b5f906e20e4",
+    "url": "/react-day-planner/static/js/main.641ae1d7.chunk.js"
+  },
+  {
+    "revision": "12a547d5be2359b88d04",
+    "url": "/react-day-planner/static/js/1.12a547d5.chunk.js"
+  },
+  {
+    "revision": "4e0358d5500a9b0b36aa",
+    "url": "/react-day-planner/static/js/runtime~main.4e0358d5.js"
+  },
+  {
+    "revision": "9ff1050f75ac44ba6dfc0e400b059822",
+    "url": "/react-day-planner/index.html"
+  }
+];
