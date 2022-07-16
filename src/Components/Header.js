75f3bd324da1604906de55b4446efdc70a32b0f1
Laciann <abhishek.acharya@apexcollege.edu.np>
2022-07-16 18:04:27 +0545

    init

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <nav> Plan your day ahead with Day Planner! </nav>
+  )
+}
+
+export default Header
---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+        <form>
+            <input type="text" placeholder='Title'/>
+            <textarea placeholder='Description'></textarea>
+            <button type="submit">ADD</button>
+        </form>
+    </div>
+  )
+}
+
+export default Home
