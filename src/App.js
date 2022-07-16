9e3274e0644710e947ad43d53433ea1e92e58b04
Laciann <abhishek.acharya@apexcollege.edu.np>
2022-07-16 20:14:43 +0545

    dep

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 const Task = ({title, description}) => {
     return (
       <div className="task"> 
